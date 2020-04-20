963f461609f3722208b55e8171bf8fba1a31dfb7
unknown <ayman.bmh123@gmail.com>
2020-04-20 20:10:26 +0000

    updating database

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ const PORT = process.env.PORT || 8080
 const db = knex({
   client : 'pg',
   connection : {
-  connectionString : process.env.DATABASE_URL,
-  ssl : true
+    connectionString: process.env.DATABASE_URL,
+    ssl: true,
   }
 });
 
