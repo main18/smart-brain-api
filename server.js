969fab4707de629d31e5d210ea8414978d79a035
unknown <ayman.bmh123@gmail.com>
2020-04-20 20:03:30 +0000

    updating database

--- server.js
+++ server.js
@@ -6,13 +6,11 @@ const knex = require('knex');
 const PORT = process.env.PORT || 8080
 
 const db = knex({
-  client: 'pg',
-  connection: {
-    host: '127.0.0.1',
-    user: 'postgres',
-    password: 'passroot',
-    database: 'smart_brain',
-  },
+  client : 'pg',
+  connection : {
+  connectionString : process.env.DATABASE_URL,
+  ssl : true
+  }
 });
 
 const app = express();
